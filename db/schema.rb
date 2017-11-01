a35be1609f1f1fd1a80f2497cd65b354863cf043
Peter Jang <peter.x.jang@gmail.com>
2017-11-01 14:49:16 -0500

    Add supplier model with updated seeds data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101191231) do
+ActiveRecord::Schema.define(version: 20171101194216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20171101191231) do
     t.decimal "price", precision: 9, scale: 2
     t.string "image"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "supplier_id"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Product.create(name: "Starburst", price: "1", image: "https://www.oldtimecandy.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/s/t/starburst-original-pkg.jpg", description: "Starburst is the brand name of a box-shaped, fruit-flavored soft taffy candy manufactured by The Wrigley Company, a subsidiary of Mars, Incorporated")
-Product.create(name: "Snickers", price: "2", image: "https://images-na.ssl-images-amazon.com/images/I/71%2Br1gAwsZL._SX466_.jpg", description: "Snickers is a brand name chocolate bar made by the American company Mars, Incorporated")
-Product.create(name: "Oreos", price: "4", image: "http://lchaimfoods.com/wp-content/uploads/2016/08/oreos.jpg", description: "Oreo is a commercial brand of cookie usually consisting of two chocolate wafers with a sweet creme filling in between")
+Supplier.create(name: "Mars", email: "mars@email.com", phone: "132432423")
+Supplier.create(name: "Nabisco", email: "nabisco@email.com", phone: "3243424")
 
+Product.create(name: "Starburst", price: "1", image: "https://www.oldtimecandy.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/s/t/starburst-original-pkg.jpg", description: "Starburst is the brand name of a box-shaped, fruit-flavored soft taffy candy manufactured by The Wrigley Company, a subsidiary of Mars, Incorporated", supplier_id: 1)
+Product.create(name: "Snickers", price: "2", image: "https://images-na.ssl-images-amazon.com/images/I/71%2Br1gAwsZL._SX466_.jpg", description: "Snickers is a brand name chocolate bar made by the American company Mars, Incorporated", supplier_id: 1)
+Product.create(name: "Oreos", price: "4", image: "http://lchaimfoods.com/wp-content/uploads/2016/08/oreos.jpg", description: "Oreo is a commercial brand of cookie usually consisting of two chocolate wafers with a sweet creme filling in between", supplier_id: 2)
