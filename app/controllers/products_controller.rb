3316eae84e15a167dd7f70b72e9d8adc40688568
Peter Jang <peter.x.jang@gmail.com>
2017-11-01 13:55:29 -0500

    Add contacts API RESTful actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,38 @@
 class ProductsController < ApplicationController
-  def all_products_method
+  def index
     products = Product.all
     render json: products.as_json
   end
 
-  def one_product_method
-    product = Product.first
+  def show
+    product = Product.find_by(id: params[:id])
     render json: product.as_json
+  end
+
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image: params[:image],
+      description: params[:description]
+    )
+    product.save
+    render json: product.as_json
+  end
+
+  def update
+    product = Product.find_by(id: params[:id])
+    product.name = params[:name] if params[:name].present?
+    product.price = params[:price] if params[:price].present?
+    product.image = params[:image] if params[:image].present?
+    product.description = params[:description] if params[:description].present?
+    product.save
+    render json: product.as_json
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: {status: "Product destroyed successfully"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get "/all_products_url" => "products#all_products_method"
-  get "/one_product_url" => "products#one_product_method" 
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 end
