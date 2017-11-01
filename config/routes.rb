72dbaa4d08550fe0a2929cd18f748a39430c7ba8
Peter Jang <peter.x.jang@gmail.com>
2017-11-01 13:37:12 -0500

    Add products api controller with (non-RESTful) get actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/all_products_url" => "products#all_products_method"
+  get "/one_product_url" => "products#one_product_method" 
 end
