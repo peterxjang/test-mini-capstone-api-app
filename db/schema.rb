94a310c576a4bf38a9c1b57e6a838824ae317f52
Peter Jang <peter.x.jang@gmail.com>
2017-11-01 14:14:18 -0500

    Change product database types

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171101182812) do
+ActiveRecord::Schema.define(version: 20171101191231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "price"
+    t.decimal "price", precision: 9, scale: 2
     t.string "image"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
